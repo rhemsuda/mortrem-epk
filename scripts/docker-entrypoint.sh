bdd07298909aec9fc1227ada4a4038f4019886a5
Kyle Jensen <kyle.jensen72@gmail.com>
2025-10-06 15:24:11 -0400

    fix syntax error

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 : "${EMAIL:=mortremofficial@gmail.com}"
 
 mkdir -p /srv/js
-printf 'window.APP_CONFIG={CDN_BASE_URL:"%s"};\n' "{CDN_BASE_URL:-}" > /srv/js/config.js
+printf 'window.APP_CONFIG={CDN_BASE_URL:"%s"};\n' "${CDN_BASE_URL:-}" > /srv/js/config.js
 
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
